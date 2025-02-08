ac249922db72df9658c8c216aafbd07ee44327e5
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2025-02-08 10:15:41 +0900

    if

--- rustlings/exercises/03_if/if1.rs
+++ rustlings/exercises/03_if/if1.rs
@@ -1,4 +1,11 @@
 fn bigger(a: i32, b: i32) -> i32 {
+    if a > b {
+        return a;
+    }else if a < b {
+        return b;
+    }else{
+        return a;
+    }
     // TODO: Complete this function to return the bigger number!
     // If both numbers are equal, any of them can be returned.
     // Do not use:
